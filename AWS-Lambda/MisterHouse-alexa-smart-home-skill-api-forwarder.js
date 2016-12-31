2456c8a660b63d8ba635e4c74798999e763d9dff
rudybrian <brudy@praecogito.com>
2016-12-30 23:40:21 -0800

    Added MisterHouse forwarder code
    I have tested with the following Lambda settings:
    Resources:
      myMisterHouseForwarder:
        Type: 'AWS::Serverless::Function'
        Properties:
          Handler: index.handler
          Runtime: nodejs4.3
          CodeUri: .
          Description: Smart Home API forwarder for MisterHouse
          MemorySize: 128
          Timeout: 10

---
+++ AWS-Lambda/MisterHouse-alexa-smart-home-skill-api-forwarder.js
@@ -0,0 +1,113 @@
+/**
+ * This sample demonstrates a simple driver  built against the Alexa Smart Home Skill Api.
+ * For additional details, please refer to the Alexa Smart Home Skill API developer documentation 
+ * https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/overviews/understanding-the-smart-home-skill-api
+ */
+var https = require('https');
+var REMOTE_CLOUD_BASE_PATH = '/mh/bin';
+var REMOTE_CLOUD_HOSTNAME = 'my.misterhousehost.com';
+
+/**
+ * Main entry point.
+ * Incoming events from Alexa Lighting APIs are processed via this method.
+ */
+exports.handler = function(event, context) {
+
+    log('Input', event);
+
+    if ((event.header.namespace === 'Alexa.ConnectedHome.Control') || (event.header.namespace === 'Alexa.ConnectedHome.Discovery')) {
+        handleNewRequest(event, context);
+    } else {
+        log('Err', 'No supported namespace: ' + event.header.namespace);
+        context.fail('Something went wrong');
+    }
+};
+
+/**
+ * This method is invoked when we receive a new message from Alexa Smart Home Skill.
+ * Forward the request on to MisterHouse and then pass the response back to the
+ * Smart Home API
+ */
+function handleNewRequest(event, context) {
+
+    var basePath = REMOTE_CLOUD_BASE_PATH + '/' + 'alexa.pl';
+
+    var options = {
+        hostname: REMOTE_CLOUD_HOSTNAME,
+        port: 443,
+        path: basePath,
+        auth: 'username:password',
+        method: 'POST',
+        headers: {
+            accept: '*/*'
+        }
+    };
+
+    var serverError = function (e) {
+        log('Error', e.message);
+        /**
+         * Craft an error response back to Alexa Smart Home Skill
+         */
+        context.fail(generateControlError(event.header.name, 'DEPENDENT_SERVICE_UNAVAILABLE', 'Unable to connect to server'));
+    };
+
+    /**
+     * Make an HTTPS call to remote endpoint.
+     */
+    var post_req = https.request(options, function(response) {
+        response.setEncoding('utf-8');
+        var str = '';
+
+        response.on('data', function(chunk) {
+            str += chunk.toString('utf-8');
+        });
+
+        response.on('end', function() {
+            /**
+            * Test the response from remote endpoint (not shown) and craft a response message
+            * back to Alexa Smart Home Skill
+            */
+            log('done with result');
+            log('Discovery', str);
+            context.succeed(JSON.parse(str));
+        });
+
+        response.on('error', serverError);
+    });
+    
+    // post the discovery request to MisterHouse
+    post_req.write(JSON.stringify(event));
+    post_req.end();
+}
+
+
+/**
+ * Utility functions.
+ */
+function log(title, msg) {
+    console.log('*************** ' + title + ' *************');
+    console.log(msg);
+    console.log('*************** ' + title + ' End*************');
+}
+
+function generateControlError(name, code, description) {
+    var headers = {
+        namespace: 'Control',
+        name: name,
+        payloadVersion: '1'
+    };
+
+    var payload = {
+        exception: {
+            code: code,
+            description: description
+        }
+    };
+
+    var result = {
+        header: headers,
+        payload: payload
+    };
+
+    return result;
+}
